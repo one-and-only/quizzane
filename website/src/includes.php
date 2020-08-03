1a724cbebbe3b589c8a3b764ac6df5ae2d1d24ad
Antonios Papadakis <bdpaboi123@gmail.com>
2020-08-03 09:14:21 -0500

    Finished Includes File

--- website/src/includes.php
+++ website/src/includes.php
@@ -1,11 +1,12 @@
+<!-- bootstrap files -->
+<link rel="stylesheet" href="src/bootstrap/css/bootstrap.min.css">
+<script src="src/bootstrap/js/jquery-3.5.1.slim.min.js"></script>
+<script src="src/bootstrap/js/bootstrap.min.js"></script>
+<script src="src/bootstrap/js/popper.min.js"></script>
+
 <?php 
 
 //stylesheet
 include('styles.css');
 //database connection
-include('dbconn.css');
-//bootstrap files
-include('bootstrap/css/bootstrap.min.css');
-includes('bootstrap/js/bootstrap.min.js');
-include('bootstrap/js/jquery-3.5.1.slim.min.js');
-include('bootstrap/js/popper.min.js');
+include('dbconn.php');
