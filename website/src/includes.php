78beddf3b3a62a27e2d97e833a5955a3c54cc944
Antonios Papadakis <bdpaboi123@gmail.com>
2020-08-03 08:30:21 -0500

    Add Bootstrap Files

--- website/src/includes.php
+++ website/src/includes.php
@@ -1,4 +1,11 @@
 <?php 
 
+//stylesheet
 include('styles.css');
+//database connection
 include('dbconn.css');
+//bootstrap files
+include('bootstrap/css/bootstrap.min.css');
+includes('bootstrap/js/bootstrap.min.js');
+include('bootstrap/js/jquery-3.5.1.slim.min.js');
+include('bootstrap/js/popper.min.js');
