761d25b66cdab9573dea89f8820746180d9e08c2
Antonios Papadakis <bdpaboi123@gmail.com>
2020-08-05 20:41:01 -0500

    Improved Header File
    The navbar search button now redirects to the game page while passing on the search query

--- website/header.php
+++ website/header.php
@@ -28,9 +28,9 @@
         <a class="nav-link disabled" href="community">Community</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    <form class="form-inline my-2 my-lg-0" method="POST" action="games">
+      <input class="form-control mr-sm-2" name="search" type="search" placeholder="Game Title..." aria-label="Search">
+      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search Games</button>
     </form>
   </div>
 </nav>
