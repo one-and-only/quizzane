f607c70bae621684c660f78152bb3014d471a504
Antonios Papadakis <bdpaboi123@gmail.com>
2020-08-06 21:53:05 -0500

    Got the Register Backend File to add a User
    Fails when trying to redirect with no error

--- website/src/registerbe.php
+++ website/src/registerbe.php
@@ -1,11 +1,15 @@
 <?php 
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include('dbconn.php');
 
-$_POST['password'] = $password;
-$passwordHASHED = password_hash($password, PASSWORD_ARGON2I);
-$_POST['email'] = $email;
-$_POST['username'] = $username;
+$_POST['passwordField1'] = $password;
+$passwordHASHED = password_hash($password, PASSWORD_BCRYPT);
+$email = $_POST['email'];
+$username = $_POST['username'];
 
 $registerSQL = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
 $registerSQL = $pdo->prepare($registerSQL);
@@ -16,12 +20,12 @@ $userInfo = [
 ];
 
 if ($registerSQL->execute($userInfo)) {
-    header(localhost);
-    exit("Registered Successfully");
+    $_SESSION['username'] = $username;
+    $_SESSION['email'] = $email;
+    header('localhost');
 }
 else {
-    header(localhost);
-    exit("An Unknown Error Occured While Registering");
+    header('localhost');
 }
 
 ?>
