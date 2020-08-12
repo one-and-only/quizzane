82a1c040845e8bb274e320e5cf2a827cb4cca8c8
Antonios Papadakis <bdpaboi123@gmail.com>
2020-08-11 22:28:06 -0500

    Create Working Register Page
    Got back to where I was before the failed disastrous merge (data loss in register branch)

---
+++ website/src/registerbe.php
@@ -0,0 +1,24 @@
+<?php
+
+include('dbconn.php');
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$registerSQL = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
+$userInfo = [
+    ':username' => $username,
+    ':email' => $email,
+    ':password' => $password
+];
+$registerSQL = $pdo->prepare($registerSQL);
+
+if ($registerSQL->execute($userInfo)) {
+    header('Location: /');
+    echo '<form method="GET"><input type="hidden" value="success"></form>';    //remove after done with debug
+    exit();
+} else {
+    header('Location: /');
+    echo '<form method="GET"><input type="hidden" value="failure"></form>';
+}
