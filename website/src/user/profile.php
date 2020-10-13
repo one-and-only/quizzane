345029326c8becd49bf6ba719c290de385b3cbcc
Antonios Papadakis <1001545@isd535.org>
2020-10-12 20:07:59 -0500

    Functioning Leaderboard For Top 5 Players
    Known Bug: Only 1st and second get colored under the medals class

--- website/src/user/profile.php
+++ website/src/user/profile.php
@@ -19,6 +19,8 @@ if ($getUserInfo->execute($username)) {
     $username = $UserInfo['username'];
     $email = $openssl->opensslDecryptAES256($UserInfo['email'], include('../opensslkey.php'));
 
+    $getLeaderboard = "SELECT * FROM users ORDER BY userScore DESC LIMIT 5";
+
     echo '
     <script>
         $(window).on("load", function() {
@@ -28,30 +30,98 @@ if ($getUserInfo->execute($username)) {
             });
         });
     </script>
+    <style>
+    * {
+        box-sizing: border-box;
+      }
+      
+      /* Create three equal columns that floats next to each other */
+      .column {
+        float: left;
+        width: 33.33%;
+        padding: 10px;
+      }
+      
+      /* Clear floats after the columns */
+      .row::after {
+        content: "";
+        display: table;
+        clear: both;
+      }
+      .table th {
+          padding: .75rem;
+          vertical-align: top;
+          border-top: 0px;
+      }
+      .medals:nth-child(1) {
+          background-color: gold;
+      }
+      .medals:nth-child(2) {
+          background-color: silver;
+      }
+      .medals:nth-child(3) {
+          background-color: bronze;
+      }
+    </style>
 
-    <div align="center" style="padding-top: 2vh;">
-        <form action="updateAccount.php" method="POST" class="modal-signature-blue" id="updateAccountForm" style="max-width: 30vw; padding-left: 1vw; padding-right: 1vw; border-radius: 30px;">
-        <div class="form-group" style="padding-top: 1vh;">
-            <label for="Email">Email address</label>
-            <input style="border-radius: 10px;" type="text" name="email" class="form-control" id="Email" aria-describedby="newEmail" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title = "Must be a valid email and less than 64 characters" placeholder="Email" value="'.$email.'" required>
+        <div class="row" style="padding-top: 2vh;">
+            <div class="column" style="padding-left: 3.5vw;">
+            <h2 align="center" style="padding-bottom: 1vh; background-color: #67e827; border-radius: 15px; color: white;">Global Leaderboard</h2>
+            
+            <table class="table modal-signature-blue" style="border-radius: 15px;">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Username</th>
+                <th scope="col">Score</th>
+                </tr>
+            </thead>
+            <tbody>';
+                $rowNum = 1;
+                foreach ($pdo->query($getLeaderboard) as $row) {
+                    if ($row['userScore'] == "") {
+                        $row['userScore'] = "0";
+                    }
+                    echo '
+                    <tr class="medals">
+                    <th scope="row">'.$rowNum.'</th>
+                    <td>'.$row['username'].'</td>
+                    <td>'.$row['userScore'].'</td>
+                    </tr>
+                    ';
+                    $rowNum++;
+                }
+            echo '
+            </tbody>
+            </table>
+
+            </div>
+            <div class="column">
+                <form action="updateAccount.php" method="POST" class="modal-signature-blue" id="updateAccountForm" style="max-width: 30vw; padding-left: 1vw; padding-right: 1vw; border-radius: 20px;">
+                <div class="form-group" style="padding-top: 1vh;">
+                    <label for="Email">Email address</label>
+                    <input style="border-radius: 10px;" type="text" name="email" class="form-control" id="Email" aria-describedby="newEmail" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title = "Must be a valid email and less than 64 characters" placeholder="Email" value="'.$email.'" required>
+                </div>
+                <div class="form-group">
+                    <label for="Username">Username</label>
+                    <input style="border-radius: 10px;" type="text" name="username" class="form-control" id="Username" placeholder="Username" value="'.$username.'" required>
+                </div>
+                <div class="form-group">
+                    <label for="newPassword">New Password</label>
+                    <input style="border-radius: 10px;" type="password" name="newPassword" class="form-control" id="newPassword" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters">
+                </div>
+                <div class="form-group">
+                    <label for="confirmNewPassword">Confirm New Password</label>
+                    <input style="border-radius: 10px;" type="password" name="confirmNewPassword" class="form-control" id="confirmNewPassword">
+                </div>
+                <div align="center" style="padding-bottom: 3vh;">
+                <button type="submit" id="updateAccountButton" class="btn-signature-green">Update Account</button>
+                </div>
+                </form>
+            </div>
+            <div class="column">
+            </div>
         </div>
-        <div class="form-group">
-            <label for="Username">Username</label>
-            <input style="border-radius: 10px;" type="text" name="username" class="form-control" id="Username" placeholder="Username" value="'.$username.'" required>
-        </div>
-        <div class="form-group">
-            <label for="newPassword">New Password</label>
-            <input style="border-radius: 10px;" type="password" name="newPassword" class="form-control" id="newPassword" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters">
-        </div>
-        <div class="form-group">
-            <label for="confirmNewPassword">Confirm New Password</label>
-            <input style="border-radius: 10px;" type="password" name="confirmNewPassword" class="form-control" id="confirmNewPassword">
-        </div>
-        <div align="center" style="padding-bottom: 3vh;">
-        <button type="submit" id="updateAccountButton" class="btn-signature-green">Update Account</button>
-        </div>
-        </form>
-    </div>
     ';
 }
 include('../../footer.php');
